de315971e26dec952485ab2e7e2aa8907e2d6300
dotn8 <dotn8@users.noreply.github.com>
2017-08-09 15:27:17 -0500

    The code for populating the database wasn't needed and was causing warnings, so remove it

--- src/main/g8/$server$/app/bootstrap/MyDBModule.scala
+++
@@ -1,9 +0,0 @@
-package bootstrap
-
-import com.google.inject.AbstractModule
-
-class MyDBModule extends AbstractModule {
-
-  protected def configure: Unit = bind(classOf[InitialData]).asEagerSingleton()
-
-}
